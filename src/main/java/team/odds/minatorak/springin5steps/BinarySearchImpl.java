63c6e4eee5a8163cf293b506c1782fbf6dc21772
minatorak <hotmail22866@gmail.com>
2018-11-05 16:09:57 +0700

    inject SortAlgorithm

--- src/main/java/team/odds/minatorak/springin5steps/BinarySearchImpl.java
+++ src/main/java/team/odds/minatorak/springin5steps/BinarySearchImpl.java
@@ -1,6 +1,11 @@
 package team.odds.minatorak.springin5steps;
 
 class BinarySearchImpl {
+    private SortAlgorithm sortAlgorithm;
+
+    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+        this.sortAlgorithm = sortAlgorithm;
+    }
 
     public int binarySearch(int[] numbers,
                             int numberToSearchFor) {
